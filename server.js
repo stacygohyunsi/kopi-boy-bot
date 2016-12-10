82d2c6b1aff648dabbbda64b67896a65063b4b5e
Joseph Matthias Goh <joseph@zephinzer.com>
2016-12-10 17:26:44 +0800

    Adding procifle and environment variable usage for Heroku config

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const http = require('http')
-const Bot = require('messenger-bot')
+const express = require('express');
+const http = require('http');
+const Bot = require('messenger-bot');
 
 let bot = new Bot({
   token: 'EAAQgsoQsJvkBAKWG6dXPficR0PNK0KkGgmce0JWuFgfBc8zJVb6X5vmBOYl79U96ATZCohRYQPXki327Pi9O3dmZAJZCQRIZC8VFXufYkXS5Jqmp93qTKZBZCD6a6bfCpBeDLZCLMlIXVJL60B7JElBUDszm57zqXII67utDPzbIwZDZD',
   verify: 'greatestbotonearth',
   app_secret: 'bc1ad72a1965e279a83d25d2b38b7270'
-})
+});
 
 bot.on('error', (err) => {
   console.log(err.message)
-})
+});
 
 bot.on('message', (payload, reply) => {
   let text = payload.message.text
@@ -23,7 +24,7 @@ bot.on('message', (payload, reply) => {
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
     })
   })
-})
+});
 
-http.createServer(bot.middleware()).listen(6787)
+http.createServer(bot.middleware()).listen(process.env.PORT);
 console.log('Kopiboy bot server running at port 6787.')
