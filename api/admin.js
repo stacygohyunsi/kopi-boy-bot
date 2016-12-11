f5b8aa2ccf32ff550d4ca725919e69d5cd969064
Stacy <stacy@meqo.com>
2016-12-11 23:01:55 +0800

    test

--- api/admin.js
+++ api/admin.js
@@ -5,21 +5,6 @@ var async = require('async');
 var exports = {};
 var router = express.Router();
 
-const config = {
-  development: {
-		apiKey: "AIzaSyAr-GmcfRJkiiOYau8zlUQYXTs8lBqGCiw",
-		authDomain: "kopiboyapp-6a596.firebaseapp.com",
-		databaseURL: "https://kopiboyapp-6a596.firebaseio.com",
-		storageBucket: "kopiboyapp-6a596.appspot.com"
-  },
-  production: {
-    apiKey: process.env.firebase_api_key,
-    authDomain: process.env.firebase_auth_domain,
-    databaseURL: process.env.firebase_database_URL, 
-		storageBucket: process.env.firebase_storage_bucket
-  },
-}
-
 firebase.initializeApp(config[process.env.NODE_ENV]);
 var db = firebase.database();
 // firebase.database.enableLogging(true);
@@ -27,27 +12,27 @@ var refCustomers = db.ref("/customers");
 var refOrders = db.ref("/orders");
 
 router.get("/orders", function(req, res, next){ 
-	refOrders.once("value", function(snapshot) {
-		var orders = snapshot.val();
-		var requestCount = 0;
+	// refOrders.on("value", function(snapshot) {
+	// 	var orders = snapshot.val();
+	// 	var requestCount = 0;
 
-		async.whilst(    
-			function () { 
-					return requestCount < Object.keys(orders).length; 
-			},function (callback) {
-				var customer = orders[Object.keys(orders)[requestCount]].customer;
-				refCustomers.child(customer).once('value', function(customerDetails) {
-						orders[Object.keys(orders)[requestCount]].customerDetails = customerDetails.val();
-						requestCount++;
-						callback();
-				});			
-			}, 
-			function (err) {
-					console.log(err);
-					res.send(orders);
-			}
-		);
-	});
+	// 	async.whilst(    
+	// 		function () { 
+	// 				return requestCount < Object.keys(orders).length; 
+	// 		},function (callback) {
+	// 			var customer = orders[Object.keys(orders)[requestCount]].customer;
+	// 			refCustomers.child(customer).once('value', function(customerDetails) {
+	// 					orders[Object.keys(orders)[requestCount]].customerDetails = customerDetails.val();
+	// 					requestCount++;
+	// 					callback();
+	// 			});			
+	// 		}, 
+	// 		function (err) {
+	// 				console.log(err);
+	// 				res.send(orders);
+	// 		}
+	// 	);
+	// });
 });
 
 exports.router = router;
--- display/js/index.js
+++ display/js/index.js
@@ -1,15 +1,27 @@
-var app = angular.module('display', []);
+var app = angular.module('display', ["firebase"]);
 
-app.controller("main", function($scope, $http) {
+app.controller("main", function($scope, $http, $firebaseObject, $firebaseArray) {
 	
-	$scope.collected = function() {
-		
-	}
-	
-	$http.get("/api/orders").then(function(resp) {
-		console.log(resp);
-		$scope.orders = resp.data;
-	}, function(err){
-		console.log(err);
-	})
+  var ref = firebase.database().ref("/orders");
+	ref.on("value", function(snapshot) {
+		console.log(snapshot.keys());
+	});
+
+  // download the data into a local object
+  $scope.orders = $firebaseArray(ref);	
+	$scope.orders.$loaded()
+  .then(function() {
+    console.log($scope.orders);
+		var privateTodosRef = $scope.orders.orderByChild("customer").equalTo(customer);
+		console.log(privateTodosRef);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+	// $http.get("/api/orders").then(function(resp) {
+	// 	console.log(resp);
+	// 	$scope.orders = resp.data;
+	// }, function(err){
+	// 	console.log(err);
+	// })
 })
