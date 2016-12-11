335d27f71f1a38f248c47074496df3c82fe33896
Stacy <stacy@meqo.com>
2016-12-12 01:19:12 +0800

    Added firebase frontend

--- display/js/index.js
+++ display/js/index.js
@@ -2,26 +2,21 @@ var app = angular.module('display', ["firebase"]);
 
 app.controller("main", function($scope, $http, $firebaseObject, $firebaseArray) {
 	
-  var ref = firebase.database().ref("/orders");
-	ref.on("value", function(snapshot) {
-		console.log(snapshot.keys());
-	});
+  var refOrders = firebase.database().ref("/orders");
+  $scope.orders = $firebaseArray(refOrders);	
 
-  // download the data into a local object
-  $scope.orders = $firebaseArray(ref);	
+	var refCustomers = firebase.database().ref("/customers");
+  $scope.customers = $firebaseArray(refCustomers);	
+
+	$scope.getCustomerName = function(id) {
+		return $scope.customers.$getRecord(id);
+	}
+	
 	$scope.orders.$loaded()
   .then(function() {
     console.log($scope.orders);
-		var privateTodosRef = $scope.orders.orderByChild("customer").equalTo(customer);
-		console.log(privateTodosRef);
   })
   .catch(function(err) {
     console.error(err);
   });
-	// $http.get("/api/orders").then(function(resp) {
-	// 	console.log(resp);
-	// 	$scope.orders = resp.data;
-	// }, function(err){
-	// 	console.log(err);
-	// })
 })
