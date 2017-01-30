b7344c4335075e920ed4393cf5a4083ed52063f4
zephinzer <dev@joeir.net>
2017-01-30 21:13:25 +0800

    Adding specifications - added should and chai packages

--- spec/kopiboy.js
+++ spec/kopiboy.js
@@ -1,3 +1,28 @@
-describe('KopiBoy build', () => {
-	it('works', () => { });
+const fs = require('fs');
+const path = require('path');
+const should = require('should');
+const {expect} = require('chai');
+
+const ACTIONS = require('../actions');
+const STRINGS = require('../strings');
+const FEATURES = require('../features');
+
+describe('KopiBoy Components', () => {
+	context('Welcome Dialog', () => {
+		context('button creators', () => {
+			it('exists', () => {
+				(() => {
+					fs.lstatSync(path.resolve('./components/buttons/welcome.js'));
+				}).should.not.throw();
+			});
+		});
+		context('strings', () => {
+			it('exists', () => {
+				expect(STRINGS.CAFE_RANDOM).to.not.be.undefined;
+				expect(STRINGS.CAFE_LIST).to.not.be.undefined;
+				expect(STRINGS.CAFE_ADD).to.not.be.undefined;
+			});
+		});
+		
+	});
 });
