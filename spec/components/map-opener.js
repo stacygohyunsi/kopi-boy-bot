1b5689e366d1017eb60533b1e6e6bdb1f5d2eedc
zephinzer <dev@joeir.net>
2017-02-02 23:29:00 +0800

    Adding tests for component:mapOpener

---
+++ spec/components/map-opener.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const {expect} = require('chai');
+
+describe('KopiBoy::Components::MapOpener', () => {
+	const expectedComponentLocation = 
+		path.join(process.cwd(), '/components/map-opener');
+	let currentModule = {};
+
+	it('is located in the right position', () => {
+		expect(() => {
+			currentModule = require(expectedComponentLocation);
+		}).to.not.throw();
+	});
+	it('implements getUrl()', () => {
+		expect(currentModule.getUrl).to.not.be.undefined;
+		expect(typeof currentModule.getUrl).to.be.equal('function');
+	});
+
+	describe('.getUrl()', () => {
+		it('takes in two arguments (latitude : Float, longitude : Float) and returns an Object<{locationFallback : String, location : String}>', () => {
+			currentModule.getUrl();
+		});
+	})
+});
