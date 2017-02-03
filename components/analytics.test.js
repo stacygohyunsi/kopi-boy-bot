4b60a59dcb0a50a5717d583e4ddddc268d2d82d7
zephinzer <dev@joeir.net>
2017-02-03 23:18:56 +0800

    Refactor

--- components/analytics.test.js
+++ components/analytics.test.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 describe('KopiBoy::Components::Analytics [#138814139]', () => {
 	const expectedComponentLocation = path.join(process.cwd(), '/components/analytics');
-	let component = {};
+	let componentExists = false;
+	let component = null;
+	try {
+		component = require(expectedComponentLocation);
+		componentExists = true;
+	} catch(ex) { }
 
 	it('is located at the right position', () => {
 		expect(() => {
@@ -11,28 +16,28 @@ describe('KopiBoy::Components::Analytics [#138814139]', () => {
 		}).to.not.throw();
 	});
 
-	it('implements sendEvent()', () => {
+	it('implements sendEvent()', componentExists ? () => {
 		expect(component.sendEvent).to.not.be.undefined;
-	});
+	} : null);
 
 	describe('.sendEvent()', () => {
-		it('throws an error if no arguments are supplied', () => {
+		it('throws an error if no arguments are supplied', componentExists ? () => {
 			expect(() => {
 				component.sendEvent();
 			}).to.throw();
-		});
-		it('can take in one to four String arguments', () => {
+		} : null);
+		it('can take in one to four String arguments', componentExists ? () => {
 			expect(() => { component.sendEvent('a'); }).to.not.throw();
 			expect(() => { component.sendEvent('a', 'b'); }).to.not.throw();
 			expect(() => { component.sendEvent('a', 'b', 'c'); }).to.not.throw();
 			expect(() => { component.sendEvent('a', 'b', 'c', 'd'); }).to.not.throw();
-		});
-		it('always uses the last argument as a callback', (done) => {
+		} : null);
+		it('always uses the last argument as a callback', component ? (done) => {
 			let asyncCallbackCount = 4;
 			component.sendEvent('a', function() { ((--asyncCallbackCount) === 0) && done(); });
 			component.sendEvent('a', 'b', function() { ((--asyncCallbackCount) === 0) && done(); });
 			component.sendEvent('a', 'b', 'c', function() { ((--asyncCallbackCount) === 0) && done(); });
 			component.sendEvent('a', 'b', 'c', 'd', function() { ((--asyncCallbackCount) === 0) && done(); });
-		});
+		} : null);
 	});
 });
