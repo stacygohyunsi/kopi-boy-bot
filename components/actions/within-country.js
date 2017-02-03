9388591756474dbde6dcd10dbadab975a57473a0
zephinzer <dev@joeir.net>
2017-02-03 23:19:13 +0800

    Refactoring into composite functions

--- components/actions/within-country.js
+++ components/actions/within-country.js
@@ -1,10 +1,84 @@
 const Sequelize = require('sequelize');
 
 const Models = require('../../models');
-
 const Strings = require('../strings');
 
+const MapOpener = require('../map-opener');
+
 const ActionWithinCountry = {
+	createBasicInfoElement: (place) => {
+		(!place.name) && (() => { throw new Error('Expected property `name` is not defined.')})();
+		(!place.address) && (() => { throw new Error('Expected property `address` is not defined.' )})();
+		// (!place.image_url) && (() => { throw new Error('Expected property `image_url` is not defined.' )})();
+		(!place.website_url) && (() => { throw new Error('Expected property `website_url` is not defined.' )})();
+		// (!place.contact_number) && (() => { throw new Error('Expected property `contact_number` is not defined.' )})();
+
+		const buttons = [{
+			type:"web_url",
+			url: place.website_url,
+			title:"View Website"
+		}];
+		(place.contact_number) && (buttons.push({
+			type: "phone_number",
+			payload: place.contact_number,
+			title: "Call Em'"
+		}))
+
+		return {
+			title: place.name,
+			image_url: place.image_url ? place.image_url : null,
+			subtitle: place.address,
+			default_action: {
+				type: "web_url",
+				url: place.website_url
+			},
+			buttons
+		};
+	},
+
+	createLocationElement: (place) => {
+		(!place.latitude) && (() => { throw new Error('Latitude is not defined.')})();
+		(!place.longitude) && (() => { throw new Error('Longitude is not defined.' )})();
+		
+		return {
+			title: 'Need Directions?',
+			subtitle: 'Check it out in your favourite maps application',
+			buttons: [
+				{
+					type: 'web_url',
+					url: MapOpener.getUrl(place.latitude, place.longitude),
+					title: 'Open in Google Maps'
+				}
+			]
+		};
+	},
+
+	createOpeningHoursElement: (place) => ({
+		title: 'Opening Hours',
+		subtitle: place.opening_hours || 'Unavailable'
+	}),
+
+	createGenericPayload: (elements) => {
+		return {
+			template_type: 'generic',
+			elements
+		};
+	},
+
+	generateReply: (place) => {
+		const elements = [ActionWithinCountry.createBasicInfoElement(place)];
+		(place.longitude && place.latitude) && elements.push(ActionWithinCountry.createLocationElement(place));
+		(place.opening_hours) && elements.push(ActionWithinCountry.createOpeningHoursElement(place));
+
+		const payload = ActionWithinCountry.createGenericPayload(elements);
+		return {
+			attachment:{
+				type: "template",
+				payload
+			}
+		};
+	},
+	
 	handle: (reply, profile) => {
 		const name = profile ? profile.name : 'dear user';
 		Models.places.find({ order: [ Sequelize.fn('RAND') ] }).then((res) => {
@@ -12,62 +86,7 @@ const ActionWithinCountry = {
 			const leadText = Strings.SUCCESS.cafeFound(name);
 			reply({ text: leadText });
 			setTimeout(() => {
-				reply({
-					"attachment":{
-						"type":"template",
-						"payload":{
-							"template_type":"generic",
-							"elements":[
-								{
-									"title": dataValues.name,
-									"image_url": null,
-									"subtitle": dataValues.address,
-									"default_action": {
-										"type": "web_url",
-										"url": dataValues.website_url
-									},
-									"buttons":[
-										{
-											"type":"web_url",
-											"url": dataValues.website_url,
-											"title":"View Website"
-										},
-										{
-											"type":"phone_number",
-											"payload": dataValues.contact_number,
-											"title":"Call Em'"
-										}
-									]      
-								},
-								{
-									title: 'Need Directions?',
-									subtitle: 'Check it out in your favourite maps application',
-									buttons: [
-										{
-											type: 'web_url',
-											url: 'https://joeir.net',
-											title: 'Open in Google Maps'
-										},
-										{
-											type: 'web_url',
-											url: 'https://joeir.net',
-											title: 'Open in Apple Maps'
-										},
-										{
-											type: 'web_url',
-											url: 'https://joeir.net',
-											title: 'Open in Maps'
-										}
-									]
-								},
-								{
-									title: 'Opening Hours',
-									subtitle: dataValues.opening_hours || 'Unavailable'
-								}
-							]
-						}
-					}
-				}, (err, info) => {
+				reply(ActionWithinCountry.generateReply(dataValues), (err, info) => {
 					console.log(err);
 					console.log(info);
 				});
