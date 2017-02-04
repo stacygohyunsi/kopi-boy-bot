a4efa8e35722182c552f402551bdd1735e9745f4
zephinzer <dev@joeir.net>
2017-02-04 18:44:06 +0800

    Renamed actions to UPPERCASE

--- components/buttons/welcome.js
+++ components/buttons/welcome.js
@@ -4,21 +4,21 @@ const Strings = require('../strings');
 
 function WelcomeButtons() {
 	const responseButtons = [];
-	if(Features.cafeRandom() === true) {
+	if(Features.CAFE_RANDOM() === true) {
 		responseButtons.push({
 			payload: Actions.CAFE_RANDOM,
 			title: Strings.CAFE_RANDOM,
 			type: 'postback',
 		});
 	}
-	if(Features.cafeList() === true) {
+	if(Features.CAFE_LIST() === true) {
 		responseButtons.push({
 			payload: Actions.CAFE_LIST,
 			title: Strings.CAFE_LIST,
 			type: 'postback',
 		});
 	}
-	if(Features.cafeAdd() === true) {
+	if(Features.CAFE_ADD() === true) {
 		responseButtons.push({
 			payload: Actions.CAFE_ADD,
 			title: Strings.CAFE_ADD,
