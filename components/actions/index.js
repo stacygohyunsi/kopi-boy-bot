5b4876595d0c37e36958a623b075b1fb7f33c310
zephinzer <dev@joeir.net>
2017-02-04 18:42:38 +0800

    Refactoring to remove cyclic dependencies

--- components/actions/index.js
+++ components/actions/index.js
@@ -1,14 +1,9 @@
 var Actions = {
 	CAFE_RANDOM: 'CAFE_RANDOM',
-	
 	CAFE_LIST: 'CAFE_LIST',
-	
 	CAFE_ADD: 'CAFE_ADD',
-	
 	WITHIN_NEARBY: 'WITHIN_NEARBY',
-
-	WITHIN_COUNTRY: 'WITHIN_COUNTRY',
-	WithinCountry: require('./within-country')
+	WITHIN_COUNTRY: 'WITHIN_COUNTRY'
 };
 
 module.exports = Actions;
