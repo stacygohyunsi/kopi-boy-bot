2000703c018b2acbfad9ce7cc7b85fb5cfc0caa8
zephinzer <dev@joeir.net>
2017-02-06 02:09:42 +0800

    fixed tests

--- components/actions/within-country.test.js
+++ components/actions/within-country.test.js
@@ -235,12 +235,10 @@ describe('KopiBoy::Components::Actions::WithinCountry', () => {
 		});
 
 		it('throws an error if input argument is not of type string', () => {
-			expect(() => {
-				expect(component.createReviewWebsitesButtons('')).to.not.throw();
-				expect(component.createReviewWebsitesButtons(1)).to.throw();
-				expect(component.createReviewWebsitesButtons({})).to.throw();
-				expect(component.createReviewWebsitesButtons(() => {})).to.throw();
-			}).to.not.throw();
+			expect(component.createReviewWebsitesButtons('')).to.not.throw();
+			expect(component.createReviewWebsitesButtons(1)).to.throw();
+			expect(component.createReviewWebsitesButtons({})).to.throw();
+			expect(component.createReviewWebsitesButtons(() => {})).to.throw();
 		});
 
 		it('returns an Object<{burpple : String, hungryGoWhere : String, yelp : String}>', () => {
