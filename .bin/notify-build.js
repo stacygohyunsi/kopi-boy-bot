11f75b14390a3d2c4b1ec49c5c0f69112bcbac40
zephinzer <dev@joeir.net>
2017-02-09 21:41:48 +0800

    Made changes to notification system

--- .bin/notify-build.js
+++ .bin/notify-build.js
@@ -2,7 +2,7 @@ const TelegramConfig = require('../config/telegram');
 const Notify = require('../components/notify');
 
 (TelegramConfig.notificationBotToken && TelegramConfig.chatId) && (() => {
-	const message = process.argv.slice(2).toString().replace(/,/gi, ' ');
+	const message = 'KopiBoy build and test process started in CodeShip!';
 	Notify('telegram', message, {
 		chatId: TelegramConfig.chatId,
 		token: TelegramConfig.notificationBotToken
