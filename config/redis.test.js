a36aa8eb9fa72c8149b7ceacd82882badb4e7b05
Stacy <stacy@meqo.com>
2017-02-11 00:33:54 +0800

    Changed config to redisConfig

--- config/redis.test.js
+++ config/redis.test.js
@@ -5,7 +5,7 @@ describe('KopiBoy::Redis', () => {
 	it('can make a connection to a Redis server', (done) => {
 		let redisClient;
 		if(process.env.IS_HEROKU) {
-			redisClient = redis.createClient(config.urlString);
+			redisClient = redis.createClient(redisConfig.urlString);
 		} else if(process.env.IS_CODESHIP) {
 			redisClient = redis.createClient(redisConfig.port, redisConfig.host);
 		} else {
