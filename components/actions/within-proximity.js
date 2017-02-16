a327163db055bb3e2beabec6ebff70d20d1f9fe2
zephinzer <dev@joeir.net>
2017-02-16 22:59:14 +0800

    😍 working version 2!

--- components/actions/within-proximity.js
+++ components/actions/within-proximity.js
@@ -5,13 +5,17 @@ const Actions = require('./index');
 const Models = require('../../models');
 const Strings = require('../strings');
 const distance = require('../location-calculator');
+const MapOpener = require('../map-opener');
+const ReviewChecker = require('../review-checker');
 
 const ProximityRandomButtons = require('../buttons/proximity-random');
 
 const {
+	generateGenericTemplateType,
 	generateListTemplateType,
 	generatePostbackButton,
-	generateTemplateAttachment
+	generateTemplateAttachment,
+	generateWebUrlButton
 } = require('./utility');
 
 
@@ -53,6 +57,41 @@ const WithinProximityAction = {
 		};
 	},
 
+
+	createBasicInfoElement: (place) => {
+		(!place.name) && (() => { throw new EvalError('Expected property `name` is not defined.')})();
+		(!place.address) && (() => { throw new EvalError('Expected property `address` is not defined.' )})();
+		// when data integrity is perfect, re-enable
+		// (!place.image_url) && (() => { throw new EvalError('Expected property `image_url` is not defined.' )})();
+		// (!place.website_url) && (() => { throw new EvalError('Expected property `website_url` is not defined.' )})();
+		// (!place.contact_number) && (() => { throw new EvalError('Expected property `contact_number` is not defined.' )})();
+
+		const buttons = [];
+		(place.website_url) && (buttons.push(generateWebUrlButton(
+			place.website_url,
+			Strings.VIEW_WEBSITE
+		)));
+		(place.latitude && place.longitude) && (buttons.push(generateWebUrlButton(
+			MapOpener.getUrl(place.latitude, place.longitude).locationFallback,
+			Strings.GET_DIRECTIONS
+		)));
+		buttons.push(generatePostbackButton(
+			Actions.WITHIN_COUNTRY_RANDOM_REPEAT,
+			Strings.SHOW_ANOTHER
+		));
+
+		return {
+			title: place.name,
+			image_url: place.image_url ? place.image_url : null,
+			subtitle: place.address,
+			default_action: {
+				type: 'web_url',
+				url: place.website_url
+			},
+			buttons
+		};
+	},
+
 	createReply: function(name) {
 		const payload = generateListTemplateType(
 			[
@@ -65,6 +104,73 @@ const WithinProximityAction = {
 			]
 		);
 
+		return generateTemplateAttachment(payload);
+	},
+
+	createOpeningHoursElement: (place) => {
+		const buttons = [];
+		(place.contact_number) && (buttons.push({
+			type: 'phone_number',
+			payload: place.contact_number,
+			title: Strings.CALL_THIS_CAFE
+		}));
+		(place.contact_email) && (buttons.push({
+			type: 'web_url',
+			payload: `mailto:${place.contact_email}`,
+			title: Strings.EMAIL_THIS_CAFE
+		}));
+
+		const element = {
+			title: 'Opening Hours',
+			subtitle: place.opening_hours || 'Unavailable. Call/email them for more information!'
+		};
+		if(buttons.length > 0) {
+			element.buttons = buttons;
+		}
+		return element;
+	},
+
+	createReviewsElement: (place) => {
+		const reviewSiteUrls = WithinProximityAction.createReviewWebsitesButtons(place.name);
+		const buttons = [{
+			type: 'web_url',
+			url: reviewSiteUrls.burpple,
+			title: Strings.CHECKOUT_BURPPLE_REVIEWS
+		},
+		{
+			type: 'web_url',
+			url: reviewSiteUrls.hungryGoWhere,
+			title: Strings.CHECKOUT_HUNGRYGOWHERE_REVIEWS
+		},
+		{
+			type: 'web_url',
+			url: reviewSiteUrls.yelp,
+			title: Strings.CHECKOUT_YELP_REVIEWS
+		}];
+
+		return {
+			title: 'Get a second opinion',
+			subtitle: 'Not sure about this place? Let\'s help you get a second opinion on...',
+			buttons
+		};
+	},
+
+	createReviewWebsitesButtons: (cafeName) => {
+		(arguments.length === 0) && (() => { throw new EvalError('Café name must be specified'); })();
+		(typeof cafeName !== 'string') && (() => { throw new EvalError('Café name must be of type String'); })();
+		return {
+			burpple: ReviewChecker.generateBurppleURL(cafeName),
+			hungryGoWhere: ReviewChecker.generateHungryGoWhereURL(cafeName),
+			yelp: ReviewChecker.generateYelpURL(cafeName)
+		}
+	},
+
+	generateReply: (place) => {
+		const elements = [WithinProximityAction.createBasicInfoElement(place)];
+		(place.opening_hours) && elements.push(WithinProximityAction.createOpeningHoursElement(place));
+		elements.push(WithinProximityAction.createReviewsElement(place));
+
+		const payload = generateGenericTemplateType(elements);
 		return generateTemplateAttachment(payload);
 	},
 
@@ -81,7 +187,7 @@ const WithinProximityAction = {
 	handle2kmRandom: function(reply, profile, callback) {
 		reply({ text: `Awesome, ${profile.first_name}, one last thing. Could you send me your location so I know where you are?` });
 		(callback) ? callback() : (() => {})();
-},
+	},
 
 	handleLocationReception: function (reply, profile, payload, coordinates, callback) {
 		(!reply) && (() => { throw new EvalError('Required parameter `reply` was not found.'); })();
@@ -100,11 +206,12 @@ const WithinProximityAction = {
 			const proximity = proximities[locationAction] ? proximities[locationAction] : null;
 			let latitudes;
 			let longitudes;
+			// console.log(coordinates);
 			if (proximity !== null) {
 				latitudes = distance.getLatitudeBounds({ latitude:coordinates.lat, longitude:coordinates.long }, proximity);
 				longitudes = distance.getLongitudeBounds({ latitude:coordinates.lat, longitude:coordinates.long }, proximity);
-				console.log(latitudes);
-				console.log(longitudes);
+				// console.log(latitudes);
+				// console.log(longitudes);
 			}
 			let text;
 			if (typeof latitudes === 'undefined' || typeof longitudes === 'undefined') {
@@ -113,22 +220,40 @@ const WithinProximityAction = {
 			} else {
 				Models.places.find({
 					where: {
-						latitude: {
-							$gt: latitudes.lowerLatitude,
-							$lt: latitudes.upperLatitude,
-							$and: {
-								longitude: {
-									$gt: longitudes.leftLongitude,
-									$lt: longitudes.rightLongtiude
-								}
-							}
-						}
+						$and: [
+							{ latitude: { $gt: latitudes.lowerLatitude } },
+							{ latitude: { $lt: latitudes.upperLatitude } },
+							{ longitude: { $gt: longitudes.leftLongitude } },
+							{ longitude: { $lt: longitudes.rightLongitude } }
+						]
 					},
 					order: [ Sequelize.fn('RAND') ]
 				}).then((res) => {
-					const { dataValues } = res;
-					console.log(dataValues);
-					(callback) ? callback() : (() => {})();
+					if(res === null) {
+						reply({
+							text: 'We couldn\'t find any cafes within your specified location ):'
+						});
+						WithinProximityAction.handleRandom(reply, profile, callback);
+					} else {
+						const { dataValues } = res;
+						const name = profile.first_name;
+						const leadText = Strings.SUCCESS.cafeFound(name);
+						reply({ text: leadText });
+						console.log(dataValues);
+						setTimeout(() => {
+							reply(WithinProximityAction.generateReply(dataValues), (err, info) => {
+								(callback) ? callback(err, info) : (() => {
+									console.log(err);
+									console.log(info);
+								})();
+							});
+						}, 500);
+					}
+				}).catch(err => {
+					console.log(err);
+					reply({
+						text:'oops'
+					});
 				});
 			}
 			//TODO: make query to the db to get the cafe
